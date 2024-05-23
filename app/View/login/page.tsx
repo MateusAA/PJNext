b7b52eb77fbc1586343ab33f041781feee73ba4a
Mateus Amancio <mateus.a.amancio@outlook.com>
2024-05-23 14:50:13 -0300

    Update dashboard layout and components, reorganize structure, and remove obsolete files

---
+++ app/View/login/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+import LoginForm from '@/app/View/login/components/LoginForm'
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h2><center>Login</center></h2>
+            <LoginForm />
+        </div>
+    );
+}
+
+export default LoginPage;
