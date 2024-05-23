2dc95cc60f3902092bdc93619c125d575776250c
Mateus Amancio <mateus.a.amancio@outlook.com>
2024-05-23 19:11:39 -0300

    m

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -22,19 +22,25 @@ export async function createInvoice(formData: FormData) {
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-      });
+    });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
+    try {
+        await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Update Invoice.'
+        };
+    }
 
 
     revalidatePath('/View/dashboard/invoices');
     redirect('/View/dashboard/invoices');
- }
+}
 
 export async function updateInvoice(id: string, formData: FormData) {
     const { customerId, amount, status } = UpdateInvoice.parse({
@@ -44,18 +50,31 @@ export async function updateInvoice(id: string, formData: FormData) {
     });
 
     const amountInCents = amount * 100;
-
-    await sql`
+    try {
+        await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Update Invoice.'
+        };
+    }
 
     revalidatePath('/View/dashboard/invoices');
     redirect('/View/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/View/dashboard/invoices');
+
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/View/dashboard/invoices');
+        return { message: 'Deleted Invoice.' };
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Delete Invoice.'
+        };
+    }
 }
