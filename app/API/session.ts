3ee7769fdd2da4aa49757a679f5ed2bca22ce22b
Ildebrando Araujo <ildebando.araujo_desenv@hotmail.com>
2024-08-30 23:00:44 -0300

    session

--- app/API/session.ts
+++ app/API/session.ts
@@ -9,12 +9,18 @@ import { SignJWT, jwtVerify } from 'jose'
 const secretKey = process.env.SESSION_SECRET
 const encodedKey = new TextEncoder().encode(secretKey)
 
-export async function encrypt(payload) {
+
+interface JWTPayload {
+    name: string;
+    id: string;
+}
+
+export async function encrypt(payload: JWTPayload) {
     return new SignJWT(payload)
         .setProtectedHeader({ alg: 'HS256' })
         .setIssuedAt()
         .setExpirationTime('7d')
-        .sign(encodedKey)
+        .sign(encodedKey);
 }
 
 
